8addb08ecf294a0b05f4bf9d51fe0c0aee0c7e29
Misha Petrov and Ron Craig <misha@>
2018-03-27 15:49:01 -0700

    first commit, edit recipe box with edit and viewing features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+
+  recipes: Recipe[] = [
+    new Recipe('Apple pie', 'Apples, Flour, Sugar, Milk, Eggs', 'Instructions...'),
+    new Recipe('Pumpkin pie', 'Pumpkin, Flour, Sugar, Milk, Eggs', 'Instructions...'),
+    new Recipe('Cheese pizza', 'Cheese, Flour, Tomato sauce, Milk, Eggs', 'Instructions...')
+  ];
+
+  selectedRecipe = null;
+
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe = clickedRecipe;
+  }
+
+  hideRecipe() {
+    this.selectedRecipe = null;
+  }
+
 }
